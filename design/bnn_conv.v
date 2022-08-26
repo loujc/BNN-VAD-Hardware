32e57e6c7b38efd1e6e18e000bd3d83f996eece4
jclou <jinchenglou@stu.pku.edu.cn>
2022-08-27 00:30:45 +0800

    更新了bnn_conv

--- design/bnn_conv.v
+++ design/bnn_conv.v
@@ -1,17 +1,16 @@
 module bnn_conv (
-    input   wire                clk     ,
-                                rst_n   ,
-    input   wire [4:0]          data_in ,//输入的一帧数据 5 位
+    input   wire                 clk           ,
+                                 rst_n         ,
+    input   wire [15:0]          data_in[1:5]  , //输入的一帧数据 5 位
 
-    output  reg signed  [2:0]   conv_out,//卷积一次输入，每一位为一个kernel和一帧输入的运算结果
-    output  reg                 conv_done//conv整个（6帧，6x20）运算结束，目前应该没用到……
+    output  reg signed  [31:0]   conv_out[1:3] , //卷积一次输入，每一位为一个kernel和一帧输入的运算结果
+    output  wire                 conv_done       //conv整个（6帧，6x20）运算结束，目前应该没用到……
 );
 
-reg signed [4:0]    weight1     ;
-reg signed [4:0]    weight2     ;
-reg signed [4:0]    weight3     ;
-reg signed [2:0]    result      ;
-reg [5:0]           cnt = 5'b0  ;
+reg signed [15:0]    weight1[1:5]       ;
+reg signed [15:0]    weight2[1:5]       ;
+reg signed [15:0]    weight3[1:5]       ;
+reg [5:0]            cnt = 5'b0         ;
 
 //权重赋值，手动键入即可
 weight1 = {1,-1,1,1,1};
@@ -20,36 +19,41 @@ weight3 = {1,-1,1,1,1};
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n)begin
-        result  <= 3'b0;
-        cnt     <= 5'b0;
+        conv_out  <= 3'b0;
     end
     else
     begin
-        result[2] <=    $signed(data_in[4]) * $signed(weight1[4])
-                    +   $signed(data_in[3]) * $signed(weight1[3])
-                    +   $signed(data_in[2]) * $signed(weight1[2])
-                    +   $signed(data_in[1]) * $signed(weight1[1])
-                    +   $signed(data_in[0]) * $signed(weight1[0]);
+        conv_out[3] <=      $signed(data_in[5]) * $signed(weight1[5])
+                        +   $signed(data_in[4]) * $signed(weight1[4])
+                        +   $signed(data_in[3]) * $signed(weight1[3])
+                        +   $signed(data_in[2]) * $signed(weight1[2])
+                        +   $signed(data_in[1]) * $signed(weight1[1]);
 
-        result[1] <=    $signed(data_in[4]) * $signed(weight2[4])
-                    +   $signed(data_in[3]) * $signed(weight2[3])
-                    +   $signed(data_in[2]) * $signed(weight2[2])
-                    +   $signed(data_in[1]) * $signed(weight2[1])
-                    +   $signed(data_in[0]) * $signed(weight2[0]);
+        conv_out[2] <=      $signed(data_in[5]) * $signed(weight2[5])
+                        +   $signed(data_in[4]) * $signed(weight2[4])
+                        +   $signed(data_in[3]) * $signed(weight2[3])
+                        +   $signed(data_in[2]) * $signed(weight2[2])
+                        +   $signed(data_in[1]) * $signed(weight2[1]);
 
-        result[0] <=    $signed(data_in[4]) * $signed(weight3[4])
-                    +   $signed(data_in[3]) * $signed(weight3[3])
-                    +   $signed(data_in[2]) * $signed(weight3[2])
-                    +   $signed(data_in[1]) * $signed(weight3[1])
-                    +   $signed(data_in[0]) * $signed(weight3[0]);
+        conv_out[1] <=      $signed(data_in[5]) * $signed(weight3[5])
+                        +   $signed(data_in[4]) * $signed(weight3[4])
+                        +   $signed(data_in[3]) * $signed(weight3[3])
+                        +   $signed(data_in[2]) * $signed(weight3[2])
+                        +   $signed(data_in[1]) * $signed(weight3[1]);
         cnt <= cnt + 5'b1;
     end
 end
 
-assign cnt = (cnt == 5'd35)? 5'b0 : cnt;
+always@(posedge clk or negedge rst_n)begin
+    if(!rst_n || (cnt == 5'd35))begin
+        cnt  <= 5'b0;
+    end
+    else begin
+        cnt  <= cnt;
+    end
+end
 
 //输出
-assign conv_out = result;
 assign conv_done = (cnt == 5'd35)? 1 : 0;
 
 endmodule //bnn_conv
