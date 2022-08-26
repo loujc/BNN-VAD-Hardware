fca079a20f845fc2cb8fe81f92a6dcca8f101c4e
jclou <jinchenglou@stu.pku.edu.cn>
2022-08-27 00:45:15 +0800

    修改了binary

--- design/binary.v
+++ design/binary.v
@@ -1,14 +1,16 @@
 module binary #(
-    parameter WIDTH      = 3;
+    parameter depth         = 32;
+    parameter target_depth  = 2;
+    parameter WIDTH         = 3;
 )
 (
-    input   wire    [WIDTH-1:0]      data_raw,
-    output  reg     [WIDTH-1:0]      data_binarized
+    input   wire    [depth-1:0]             data_raw[WIDTH-1:0]        ,
+    output  reg     [target_depth-1:0]      data_binarized[WIDTH-1:0]
 );
 
 always @(*) begin
     for(count = 0; count <= WIDTH; count = count + 1)begin
-        data_binarized[WIDTH] = (data_raw[WIDTH] == 0)? 0 :(data_raw[WIDTH] >= 1)? 1 : 0;
+        data_binarized[WIDTH] = (data_raw[WIDTH] >= 0)? 1 : -1;
     end
 end
 
