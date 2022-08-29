d676fa90a767e1e2e23481d55f82710d016ce82d
jclou <jinchenglou@stu.pku.edu.cn>
2022-08-29 21:29:27 +0800

    整个替换了binary

--- design/binary.v
+++ design/binary.v
@@ -1,17 +1,26 @@
 module binary #(
-    parameter depth         = 32;
-    parameter target_depth  = 2;
-    parameter WIDTH         = 3;
+    parameter depth         = 32,
+    parameter target_depth  = 2,
+    parameter WIDTH         = 3
 )
 (
-    input   wire    [depth-1:0]             data_raw[WIDTH-1:0]        ,
+    input   reg     [depth-1:0]             data_raw[WIDTH-1:0]        ,
     output  reg     [target_depth-1:0]      data_binarized[WIDTH-1:0]
 );
 
-always @(*) begin
-    for(count = 0; count <= WIDTH; count = count + 1)begin
-        data_binarized[WIDTH] = (data_raw[WIDTH] >= 0)? 1 : -1;
+genvar cnt;
+generate
+    for (cnt = 0; cnt < WIDTH; cnt=cnt+1)
+    begin: binaryLoop
+        always @(*) begin
+            if(data_raw[cnt] >= 'b0) begin
+                data_binarized[cnt] = 1;
+            end
+            else begin
+                data_binarized[cnt] = -1;
+            end
+        end
     end
-end
+endgenerate
 
 endmodule //binary
