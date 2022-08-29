73473ac6425c38c57e2b3c15e29b4d4bbad9c8c5
jclou <jinchenglou@stu.pku.edu.cn>
2022-08-29 14:03:39 +0800

    修改了compare input

--- design/compare.v
+++ design/compare.v
@@ -2,8 +2,8 @@ module compare (
     input   wire                clk         ,
                                 rst_n       ,
                                 enable      ,//使能比较，即前面运算部分全部结束
-    input   wire [1:0]          compare_in  ,//输入1x2数据
-
+    input   wire [3:0]          compare_in[1:0]  ,//输入1x2数据,目前依旧使用4位位宽
+                                                  // TODO：如何调整位宽为2，一个应该取原数据高2位还是低2位
     output reg  [1:0]           result      //输出分类结果，10为1类别，01为2类别，00为无结果
 );
 
