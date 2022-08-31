d6e28be94b4af7921c4d273531882930cc1b5874
jclou <jinchenglou@stu.pku.edu.cn>
2022-08-31 18:46:58 +0800

    update bnn_conv 2 server version

--- design/bnn_conv.v
+++ design/bnn_conv.v
@@ -3,24 +3,26 @@ module bnn_conv (
                                  rst_n         ,
     input   wire [15:0]          data_in[1:5]  , //输入的一帧数据 5 位
 
-    output  reg signed  [31:0]   conv_out[1:3] , //卷积一次输入，每一位为一个kernel和一帧输入的运算结果
+    output  reg  [31:0]   conv_out[1:3] , //卷积一次输入，每一位为一个kernel和一帧输入的运算结果
     output  wire                 conv_done       //conv整个（6帧，6x20）运算结束，目前应该没用到……
 );
 
-reg signed [15:0]    weight1[1:5]       ;
-reg signed [15:0]    weight2[1:5]       ;
-reg signed [15:0]    weight3[1:5]       ;
-reg [5:0]            cnt = 5'b0         ;
+reg [15:0]    weight1[1:5]       ;
+reg [15:0]    weight2[1:5]       ;
+reg [15:0]    weight3[1:5]       ;
+reg [5:0]            cnt         ;
 
 //权重赋值，手动键入即可
-weight1 = {1,-1,1,1,1};
-weight2 = {1,-1,1,1,1};
-weight3 = {1,-1,1,1,1};
+
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n)begin
-        conv_out  <= 3'b0;
-    end
+	cnt	<=	0;
+
+	weight1 <= {1,-1,1,1,1};
+	weight2 <= {1,-1,1,1,1};
+	weight3 <= {1,-1,1,1,1};    
+end
     else
     begin
         conv_out[3] <=      $signed(data_in[5]) * $signed(weight1[5])
